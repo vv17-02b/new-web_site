66ea11cb0d1b27ebbc8418435023503248192f06
Vladimir <bessv6591@gmail.com>
2025-02-03 19:23:12 +0200

    Add files via upload

---
+++ new-web_site/js/scrool-site.js
@@ -0,0 +1,15 @@
+
+// розділ анкерів anchors on section
+const anchors = document.querySelectorAll('a[href*="#"]');
+anchors.forEach(anchor => {
+  anchor.addEventListener('click', event => {
+    event.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substring(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+
+});
